016bb8e88a9632df775bd227ecf38459a92ff789
SarahBarbosa <sarah@fisica.ufc.br>
2024-04-07 21:48:34 -0300

    add tqdm

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ from setuptools import setup, find_packages
 setup(name="geexhp", 
       version="1.0", 
       packages=find_packages(), 
-      install_requires=["numpy","matplotlib","pandas","msgpack","astropy", "fastparquet", "PyQt5"])
+      install_requires=["numpy","matplotlib","pandas","msgpack","astropy", "fastparquet", "PyQt5", "tqdm"])
