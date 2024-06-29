d5bfa11d00935bea3a71ac03b0869f2cf1518d84
SarahBarbosa <sarah@fisica.ufc.br>
2024-06-29 17:03:31 -0300

    datagen_v2.1

--- geexhp/__init__.py
+++ geexhp/__init__.py
@@ -1,5 +1,25 @@
-from .core.datagen import DataGen
-from .core.datavis import DataVis
+from .core.datagen import *
+from .core.datavis import *
+from .core.datamod import *
 
-__version__ = "0.0.1"
-__all__ = ["DataGen", "DataVis"]
+__version__ = "1.0.0"
+__all__ = [
+    'DataGen',
+    'get_config',
+    'modern_earth',
+    'configure_matplotlib',
+    'plot_spectrum',
+    'label_line',
+    'mixing_ratio_constant',
+    'random_atmospheric_layers',
+    'normalize_layer',
+    'set_spectral_type',
+    'set_stellar_parameters',
+    'set_solar_coordinates',
+    'set_atmospheric_pressure',
+    'calculate_luminosity',
+    'set_habitable_zone_distance',
+    'maintain_planetary_atmosphere',
+    'set_instrument',
+    'random_planet'
+]
