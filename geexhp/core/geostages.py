77bb45e9c313dd4492cc45b6d4ea3663530da753
SarahBarbosa <sarah@fisica.ufc.br>
2024-06-29 17:03:31 -0300

    datagen_v2.1

--- geexhp/core/geostages.py
+++ geexhp/core/geostages.py
@@ -1,33 +1,30 @@
 import numpy as np
 
-def terra_moderna(dicionario):
+def modern_earth(config: dict) -> None:
     """
-    Calcula parâmetros atmosféricos para uma Terra moderna.
+    Calculate atmospheric parameters for a modern Earth simulation.
 
-    Parâmetros:
-    -----------
-    dicionario : dict
-        Dicionário para armazenar os parâmetros atmosféricos calculados.
+    Parameters
+    ----------
+    config : dict
+        A dictionary to store the calculated atmospheric parameters.
 
-    Retorno:
-    --------
+    Returns
+    -------
     None
 
-    Notas:
-    ------
-    Esta função calcula os parâmetros atmosféricos de uma Terra moderna,
-    incluindo a pressão, a temperatura e a abundância de diversos gases em diferentes camadas atmosféricas.
-
-    As camadas atmosféricas são modeladas com base em valores fixos de pressão e temperatura,
-    enquanto a abundância dos gases varia em cada camada.
-
-    A abundância dos gases é fornecida como uma fração em relação ao total da mistura em cada camada.
+    Notes
+    -----
+    This function models atmospheric parameters of a modern Earth, including pressure, 
+    temperature, and the abundance of various gases in different atmospheric layers.
+    Atmospheric layers are modeled based on fixed values of pressure and temperature,
+    while the abundance of gases varies in each layer.
+    Gas abundances are provided as a fraction relative to the total mixture in each layer.
     """
+    layers = 60
 
-    camadas = 60
-
-    # Valores fixos iguais da Terra
-    pressao_fixa = np.array([1.013e+00, 8.988e-01, 7.950e-01, 7.012e-01, 6.166e-01, 5.405e-01,
+    # Fixed values similar to Earth
+    pressure = np.array([1.013e+00, 8.988e-01, 7.950e-01, 7.012e-01, 6.166e-01, 5.405e-01,
                         4.722e-01, 4.111e-01, 3.565e-01, 3.080e-01, 2.650e-01, 2.270e-01,
                         1.940e-01, 1.658e-01, 1.417e-01, 1.211e-01, 1.035e-01, 8.850e-02,
                         7.565e-02, 6.467e-02, 5.529e-02, 4.729e-02, 4.047e-02, 3.467e-02,
@@ -37,19 +34,19 @@ def terra_moderna(dicionario):
                         4.460e-06, 1.840e-06, 7.600e-07, 3.200e-07, 1.450e-07, 7.100e-08,
                         4.010e-08, 2.540e-08, 6.023e-09, 2.614e-09, 1.337e-09, 7.427e-10,
                         4.386e-10, 2.681e-10, 1.693e-10, 1.097e-10, 7.234e-11, 4.900e-11])
-    temperatura_fixa = np.array([ 288.2,  281.7,  275.2,  268.7,  262.2,  255.7,  249.2,  242.7,
-                                236.2,  229.7,  223.3,  216.8,  216.7,  216.7,  216.7,  216.7,
-                                216.7,  216.7,  216.7,  216.7,  216.7,  217.6,  218.6,  219.6,
-                                220.6,  221.6,  224. ,  226.5,  229.6,  236.5,  243.4,  250.4,
-                                257.3,  264.2,  270.6,  270.7,  260.8,  247. ,  233.3,  219.6,
-                                208.4,  198.6,  188.9,  186.9,  188.4,  195.1,  208.8,  240. ,
-                                300. ,  360. ,  610. ,  759. ,  853. ,  911. ,  949. ,  973. ,
-                                988. ,  998. , 1000. , 1010. ])
+    temperature = np.array([ 288.2,  281.7,  275.2,  268.7,  262.2,  255.7,  249.2,  242.7,
+                            236.2,  229.7,  223.3,  216.8,  216.7,  216.7,  216.7,  216.7,
+                            216.7,  216.7,  216.7,  216.7,  216.7,  217.6,  218.6,  219.6,
+                            220.6,  221.6,  224. ,  226.5,  229.6,  236.5,  243.4,  250.4,
+                            257.3,  264.2,  270.6,  270.7,  260.8,  247. ,  233.3,  219.6,
+                            208.4,  198.6,  188.9,  186.9,  188.4,  195.1,  208.8,  240. ,
+                            300. ,  360. ,  610. ,  759. ,  853. ,  911. ,  949. ,  973. ,
+                            988. ,  998. , 1000. , 1010. ])
     
-    # Razões de abundância para várias moléculas
-    CO2 = np.full(camadas, 3.795e-04)
-    N2 = np.full(camadas, 0.781)
-    O2 = np.full(camadas, 0.209)
+    # Gas abundance ratios for various molecules
+    CO2 = np.full(layers, 3.795e-04)
+    N2 = np.full(layers, 0.781)
+    O2 = np.full(layers, 0.209)
     H2O = np.array([7.745e-03, 6.071e-03, 4.631e-03, 3.182e-03, 2.158e-03, 1.397e-03,
                     9.254e-04, 5.720e-04, 3.667e-04, 1.583e-04, 6.996e-05, 3.613e-05,
                     1.906e-05, 1.085e-05, 5.927e-06, 5.000e-06, 3.950e-06, 3.850e-06,
@@ -110,8 +107,8 @@ def terra_moderna(dicionario):
                     5.000e-07, 7.000e-07, 7.000e-07, 4.000e-07, 2.000e-07, 5.000e-08,
                     5.000e-09, 5.000e-10, 0.000e+00, 0.000e+00, 0.000e+00, 0.000e+00,
                     0.000e+00, 0.000e+00, 0.000e+00, 0.000e+00, 0.000e+00, 0.000e+00])
-    CH4 = np.full(camadas, 1.700e-06)
-    N2O = np.full(camadas, 3.200e-07)
+    CH4 = np.full(layers, 1.700e-06)
+    N2O = np.full(layers, 3.200e-07)
     NH3 = np.array([5.00e-10, 5.00e-10, 4.63e-10, 3.80e-10, 2.88e-10, 2.04e-10,
                     1.46e-10, 9.88e-11, 6.48e-11, 3.77e-11, 2.03e-11, 1.09e-11,
                     6.30e-12, 3.12e-12, 1.11e-12, 4.47e-13, 2.11e-13, 1.10e-13,
@@ -133,49 +130,48 @@ def terra_moderna(dicionario):
                     3.59e-13, 3.56e-13, 0.00e+00, 0.00e+00, 0.00e+00, 0.00e+00,
                     0.00e+00, 0.00e+00, 0.00e+00, 0.00e+00, 0.00e+00, 0.00e+00])                 
 
-    # Moléculas adicionais com abundância fixa (2%)
-    H2 = np.full(camadas, 0.02)
-    H2S = np.full(camadas, 0.02)
-
-    # Informação sobre as moléculas (https://pt.webqc.org/mmcalc.php)
-    moleculas = ["CO2" , "N2" , "O2" , "H2O", "CO", "H2" , "C2H6" , "HCN", 
-                 "H2S", "SO2" , "O3" , "CH4" , "N2O", "NH3" , "CH3Cl"]
-    peso_molecular = [44.0095, 28.01340, 31.99880, 18.01528, 28.0101, 2.01588, 
-                      30.0690, 27.0253, 34.0809, 64.0638, 47.99820, 16.0425, 44.01280, 
-                      17.03052, 50.4875]
+    # Additional molecules with fixed abundance (2%)
+    H2 = np.full(layers, 0.02)
+    H2S = np.full(layers, 0.02)
     
-    # Preenchendo o dicionário com dados atmosféricos
-    for i in range(camadas):
-        total_abundancia_camada = np.sum([
+    # Populate the dictionary with atmospheric data
+    for i in range(layers):
+        total_layer_abundance = np.sum([
             CO2[i], N2[i], O2[i], H2O[i], CO[i], H2[i], C2H6[i], HCN[i], H2S[i], SO2[i],
             O3[i], CH4[i], N2O[i], NH3[i], CH3Cl[i]
         ])
         
-        dicionario[f'ATMOSPHERE-LAYER-{i + 1}'] = ','.join([
-            str(pressao_fixa[i]), str(temperatura_fixa[i]),
-            *[str(abundancia / total_abundancia_camada) for abundancia in [
+        config[f'ATMOSPHERE-LAYER-{i + 1}'] = ','.join([
+            str(pressure[i]), str(temperature[i]),
+            *[str(abundance / total_layer_abundance) for abundance in [
                 CO2[i], N2[i], O2[i], H2O[i], CO[i], H2[i], C2H6[i], HCN[i], H2S[i], SO2[i],
                 O3[i], CH4[i], N2O[i], NH3[i], CH3Cl[i]
             ]]
         ])
+
+    # Information about molecules (Reference: https://pt.webqc.org/mmcalc.php)
+    molecules = ["CO2" , "N2" , "O2" , "H2O", "CO", "H2" , "C2H6" , "HCN", 
+                    "H2S", "SO2" , "O3" , "CH4" , "N2O", "NH3" , "CH3Cl"]
+    molecular_weight = np.array([44.0095, 28.01340, 31.99880, 18.01528, 28.0101, 2.01588, 
+                                    30.0690, 27.0253, 34.0809, 64.0638, 47.99820, 16.0425, 44.01280, 
+                                    17.03052, 50.4875])
     
-    # Pegando a abundância média apenas da primeira camada (isso é uma aproximação!)
-    molweights = np.array(peso_molecular)
-    abuns = np.array([CO2[0], N2[0], O2[0], H2O[0], CO[0], H2[0], C2H6[0], HCN[0], H2S[0], SO2[0], O3[0], CH4[0], 
-                        N2O[0], NH3[0], CH3Cl[0]])
-    peso_molecular_medio = np.sum(molweights * abuns)
+    # Calculating average abundance from the first layer (as an approximation)
+    abundances = np.array([CO2[0], N2[0], O2[0], H2O[0], CO[0], H2[0], C2H6[0], 
+                            HCN[0], H2S[0], SO2[0], O3[0], CH4[0], N2O[0], NH3[0], CH3Cl[0]])
+    average_molecular_weight = np.sum(molecular_weight * abundances)
     
-    # Dicionário de mapeamento de moléculas para índices de banco de dados HITRAN
+    # Mapping molecules to HITRAN database indices
     # https://hitran.org/lbl/
-    HITDICT = {"CO2": "HIT[2]", "N2" : "HIT[22]", "O2" : "HIT[7]", 
-                "H2O": "HIT[1]", "CO": "HIT[5]", "H2": "HIT[45]", "C2H6": "HIT[27]",
-                "HCN": "HIT[23]", "H2S": "HIT[31]", "SO2": "HIT[9]", "O3":"HIT[3]", 
-                "CH4" :"HIT[6]", "N2O": "HIT[4]", "NH3" : "HIT[11]", "CH3Cl": "HIT[24]"}
+    HITRAN_DICT = {"CO2": "HIT[2]", "N2" : "HIT[22]", "O2" : "HIT[7]", 
+                    "H2O": "HIT[1]", "CO": "HIT[5]", "H2": "HIT[45]", "C2H6": "HIT[27]",
+                    "HCN": "HIT[23]", "H2S": "HIT[31]", "SO2": "HIT[9]", "O3":"HIT[3]", 
+                    "CH4" :"HIT[6]", "N2O": "HIT[4]", "NH3" : "HIT[11]", "CH3Cl": "HIT[24]"}
     
-    dicionario['ATMOSPHERE-WEIGHT'] = peso_molecular_medio
-    dicionario['ATMOSPHERE-NGAS'] = len(moleculas)
-    dicionario['ATMOSPHERE-GAS'] = ",".join(moleculas)
-    dicionario['ATMOSPHERE-TYPE'] = ",".join([HITDICT[i] for i in moleculas]) 
-    dicionario['ATMOSPHERE-ABUN'] = "1,"*(len(moleculas)-1)+ '1'
-    dicionario['ATMOSPHERE-UNIT'] = "scl,"*(len(moleculas)-1)+ 'scl' 
-    dicionario['ATMOSPHERE-LAYERS-MOLECULES'] = ','.join(moleculas)
+    config['ATMOSPHERE-WEIGHT'] = average_molecular_weight
+    config['ATMOSPHERE-NGAS'] = len(molecules)
+    config['ATMOSPHERE-GAS'] = ",".join(molecules)
+    config['ATMOSPHERE-TYPE'] = ",".join([HITRAN_DICT[mol] for mol in molecules]) 
+    config['ATMOSPHERE-ABUN'] = "1,"*(len(molecules)-1) + '1'
+    config['ATMOSPHERE-UNIT'] = "scl,"*(len(molecules)-1) + 'scl' 
+    config['ATMOSPHERE-LAYERS-MOLECULES'] = ','.join(molecules)
