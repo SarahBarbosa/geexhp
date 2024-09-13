6c9309586ddb8acf3cce4e48cc70332173871119
SarahBarbosa <sarah@fisica.ufc.br>
2024-09-13 14:03:00 -0300

    monte carlo dropout and new model - not the best

--- geexhp/__init__.py
+++ geexhp/__init__.py
@@ -1,5 +1,5 @@
 from .core import datavis, datagen, datamod, stages
-from .model import cnn1d, datasetup
+from .model import cnn, datasetup
 
 __version__ = "1.0.0"
 __all__ = ["datavis", "datagen", "datamod", "stages", "datasetup"]
